5a19c4239793c11f1d82d92322cf10aaa320bac8
zaharevic <zaharvahterov@gmail.com>
2024-08-02 15:19:12 +0400

    Исправил ошибки

---
+++ project/homework1/src/main/java/com/example/homework1/Application.java
@@ -0,0 +1,15 @@
+import model.Kotik;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Application {
+    public static void main(String[] args) {
+        Kotik kot1 = new Kotik(73, "Вася", 12, "мяу");
+        Kotik kot2 = new Kotik();
+        kot2.setKotik(81, "Рыжий", 15, "мяу!!");
+        kot1.liveAnotherDay();
+        kot2.printStat();
+        System.out.println(kot1.getMeow().equals(kot2.getMeow()));
+        System.out.println(Kotik.getCatsCounter());
+    }
+}
